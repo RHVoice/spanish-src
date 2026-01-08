f0d6261619a3fd08758111a559ea810a1cbcbac0
Mateito <angelitomateocedillo@gmail.com>
2026-01-07 22:08:45 -0500

    Post g2p: fixed a minor number stressing case.

--- check_equals.py
+++ check_equals.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2024  Mateo Cedillo <angelitomateocedillo@gmail.com>
+# Copyright (C) 2024-2026  Mateo Cedillo <angelitomateocedillo@gmail.com>
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published by
@@ -23,10 +23,12 @@ def print_differences(file1, file2):
 		text2 = f2.readlines()
 	assert len(text1) == len(text2), "No se puede hacer la diferencia porque ambos archivos no tienen el mismo número de líneas"
 	for i, (line1, line2) in enumerate(zip(text1, text2), start=1):
+		line1 = line1.strip()
+		line2 = line2.strip()
 		ratio = difflib.SequenceMatcher(None, line1, line2).ratio()
 		if ratio < 1.0:
 			print(f"Diferencia en la línea {i}, detalles a continuación:\nCopia: {line1}\nActual: {line2}\n")
+	print("Listo, patrón.")
 
 if __name__ == "__main__":
 	print_differences(argv[1], argv[2])
-	print("Listo, patrón.")
